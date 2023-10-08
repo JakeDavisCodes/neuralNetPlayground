1040b3c81b94824121c05ff5b8b3d3e621be94ba
Jake <jakedavisemail@gmail.com>
2023-10-08 15:33:43 -0600

    Neuron.activate

--- classes/node.js
+++ classes/node.js
@@ -5,5 +5,15 @@ class Neuron {
     this.bias = Math.random()
   }
 
+  activate() {
+    let sum = 0;
 
+    for (let i = 0; i < inputs.length; i++) {
+      sum += this.inputs[i] * this.weights[i];
+    }
+
+    sum += this.bias
+
+    return 1 / (1 + Math.exp(-sum))
+  }
 }
